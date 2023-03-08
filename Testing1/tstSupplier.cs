e3fdc2e1cff85562ea9a026e5c80910704d4db61
ULUCBEY TEREK <P2664516@my365.dmu.ac.uk>
2023-03-08 01:27:22 +0000

    Testing completed for supplier

--- Testing1/tstSupplier.cs
+++ Testing1/tstSupplier.cs
@@ -13,5 +13,68 @@ namespace Testing1
             clsSupplier supplier = new clsSupplier();
             Assert.IsNotNull(supplier);
         }
+
+        [TestMethod]
+        public void Order_datePropertyOK()
+        {
+            clsSupplier supplier = new clsSupplier();
+            DateTime TestData = DateTime.Now.Date;
+            supplier.Order_date = TestData;
+            Assert.AreEqual(supplier.Order_date, TestData);
+       
+        }
+        [TestMethod]
+        public void SupplierIdPropertyOK()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Int32 TestData = 1;
+            supplier.SupplierId = TestData;
+            Assert.AreEqual(supplier.SupplierId, TestData);
+        }
+        [TestMethod]
+        public void Company_nameOK()
+        {
+            clsSupplier supplier = new clsSupplier();
+            String TestData = "versace";
+            supplier.Company_name = TestData;
+            Assert.AreEqual(supplier.Company_name, TestData);
+        }
+        [TestMethod]
+        public void Company_addressOK()
+        {
+            clsSupplier supplier = new clsSupplier();
+            String TestData = "LO2 3BE";
+            supplier.Company_address = TestData;
+            Assert.AreEqual(supplier.Company_address, TestData);
+        }
+        [TestMethod]
+        public void Contact_numberOK()
+        {
+            clsSupplier supplier = new clsSupplier();
+            String TestData = "05554152";
+            supplier.Contact_number = TestData;
+            Assert.AreEqual(supplier.Contact_number, TestData);
+        }
+        [TestMethod]
+        public void Unit_costOK()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Int32 TestData = 155;
+            supplier.Unit_cost = TestData;
+            Assert.AreEqual(supplier.Unit_cost, TestData);
+        }
+        [TestMethod]
+        public void Perfume_IDOK()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Int32 TestData = 1;
+            supplier.Perfume_ID = TestData;
+            Assert.AreEqual(supplier.Perfume_ID, TestData);
+        }
+
+
+
+
+
     }
 }
