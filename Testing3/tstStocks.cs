d748b70340a9920b949885099507190e970f364e
Haseeb1567 <haseebasaduk@gmail.com>
2023-03-09 20:23:21 +0000

    Added testing for Stocks class

---
+++ Testing3/tstStocks.cs
@@ -0,0 +1,73 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStocks
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStocks stocks = new clsStocks();
+            Assert.IsNotNull(stocks);
+        }
+
+        [TestMethod]
+        public void StocksDeliveryStatusPropertyOK()
+        {
+            clsStocks stocks = new clsStocks();
+            Boolean TestData = true;
+            stocks.StocksDeliveryStatus = TestData;
+            Assert.AreEqual(stocks.StocksDeliveryStatus, TestData);
+        }
+
+        [TestMethod]
+        public void ProductQuantityPropertyOK()
+        {
+            clsStocks stocks = new clsStocks();
+            Int32 TestData = 10;
+            stocks.ProductQuantity = TestData;
+            Assert.AreEqual(stocks.ProductQuantity, TestData);
+        }
+
+        [TestMethod]
+        public void PruductPricePropertyOK()
+        {
+            clsStocks stocks = new clsStocks();
+            float TestData = 100;
+            stocks.PruductPrice = TestData;
+            Assert.AreEqual(stocks.PruductPrice, TestData);
+        }
+
+        [TestMethod]
+        public void Stock_In_DatePropertyOK()
+        {
+            clsStocks stocks = new clsStocks(); 
+            DateTime TestData = DateTime.Now.Date;
+            stocks.Stock_In_Date = TestData;
+            Assert.AreEqual(stocks.Stock_In_Date, TestData);
+        }
+
+        [TestMethod]
+        public void Stock_Out_DatePropertyOK()
+        {
+            clsStocks stocks = new clsStocks();
+            DateTime TestData = DateTime.Now.Date;
+            stocks.Stock_Out_Date = TestData;
+            Assert.AreEqual(stocks.Stock_Out_Date, TestData);
+        }
+
+        [TestMethod]
+        public void Stock_BrandNamePropertyOK()
+        {
+            clsStocks stocks = new clsStocks();
+            string TestData = "Boss";
+            stocks.Stock_BrandName = TestData;
+            Assert.AreEqual(stocks.Stock_BrandName, TestData);
+        }
+
+    }
+}
+
