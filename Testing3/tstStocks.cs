a8c204dc77d05b3feb80aae04ecef8fc4aed3cbf
Haseeb1567 <haseebasaduk@gmail.com>
2023-03-09 20:23:21 +0000

    Added testing for Stocks class

---
+++ Testing3/tstStocks.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStocks
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStocks stocks = new clsStocks();
+            Assert.IsNotNull(stocks);
+        }
+    }
+}
