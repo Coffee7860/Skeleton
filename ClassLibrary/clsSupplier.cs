8bc1a46cc0a68902f9ec6d635938e01defae5561
ULUCBEY TEREK <P2664516@my365.dmu.ac.uk>
2023-03-21 14:02:36 +0000

    Finding method completed except stored procedure

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -4,12 +4,100 @@ namespace ClassLibrary
 {
     public class clsSupplier
     {
-        public DateTime OrderDate { get; set; }
-        public int SupplierID { get; set; }
-        public string CompanyName { get; set; }
-        public string CompanyAddress { get; set; }
-        public string ContactNumber { get; set; }
-        public int UnitCost { get; set; }
-        public bool Location { get; set; }
+        public DateTime OrderDate 
+        {
+            get 
+            {
+                return mOrderDate;
+            }
+            set 
+            {
+                mOrderDate = value;
+            } 
+        }
+        public Int32 SupplierID 
+        {
+            get
+            {
+                return mSupplierId;
+            }
+            set
+            {
+                mSupplierId = value;
+            }
+        }
+        public string CompanyName 
+        {
+            get
+            {
+                return mCompanyName;
+            }
+          set
+            {
+                mCompanyName = value;
+            }
+        }
+        public string CompanyAddress
+        { 
+            get 
+            {
+                return mCompanyAddress;
+            }
+            set
+            {
+                mCompanyAddress = value;
+            } 
+        }
+        public string ContactNumber 
+        {
+            get 
+            {
+                return mContactNumber;
+            }
+            set
+            {
+                mContactNumber = value;
+            } 
+        }
+        public int UnitCost 
+        {
+            get
+            {
+                return mUnitCost;
+            }
+            set
+            {
+                mUnitCost = value;
+            }
+        }
+        public bool Location {
+            get
+            {
+                return mLocation;
+            }
+            set
+            {
+                mLocation = value;
+            }
+        }
+
+        public bool Find(int supplierId)
+        {
+            mSupplierId = 2;
+            mOrderDate = Convert.ToDateTime("01 / 02 / 2023");
+            mCompanyName = "Test Town";
+            mCompanyAddress = "LE3 5JK";
+            mContactNumber = "0000";
+            mUnitCost = 150;
+            mLocation = true;
+            return true;
+        }
+        private Int32 mSupplierId;
+        private DateTime mOrderDate;
+        private string mCompanyName;
+        private string mCompanyAddress;
+        private string mContactNumber;
+        private Int32 mUnitCost;
+        private bool mLocation;
     }
 }
--- Testing1/tstSupplier.cs
+++ Testing1/tstSupplier.cs
@@ -71,6 +71,115 @@ namespace Testing1
             supplier.Location = TestData;
             Assert.AreEqual(supplier.Location, TestData);
         }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Boolean Found = false;
+            Int32 SupplierId = 2;
+            Found = supplier.Find(SupplierId);
+            Assert.IsTrue(Found);
+        }
+        [TestMethod]
+        public void TestSupplierIdNoFound()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SupplierId = 2;
+            Found = supplier.Find(SupplierId);
+            if(supplier.SupplierID !=2)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestOrderDateFound()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SupplierId = 2;
+            Found = supplier.Find(SupplierId);
+            if (supplier.OrderDate != Convert.ToDateTime("01 / 02 / 2023"))
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestCompanyNameFound()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SupplierId = 2;
+            Found = supplier.Find(SupplierId);
+            if (supplier.CompanyName != "Test Town")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestCompanyAddressFound()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SupplierId = 2;
+            Found = supplier.Find(SupplierId);
+            if (supplier.CompanyAddress != "LE3 5JK")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestContactNumberFound()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SupplierId = 2;
+            Found = supplier.Find(SupplierId);
+            if (supplier.ContactNumber != "0000")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestUnitCostFound()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SupplierId = 2;
+            Found = supplier.Find(SupplierId);
+            if (supplier.UnitCost != 150)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestLocationFound()
+        {
+            clsSupplier supplier = new clsSupplier();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 SupplierId = 2;
+            Found = supplier.Find(SupplierId);
+            if (supplier.Location != true)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+
+
 
 
 
